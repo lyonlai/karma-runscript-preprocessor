d402f60f8cf99b286fabf6b2234e36460cc59978
Yun Lai <lyonlai1984@gmail.com>
2015-06-24 21:38:43 +1000

    initial commit.

---
+++ index.js
@@ -0,0 +1,39 @@
+var shell = require('shelljs');
+
+var runScriptOnChangePreprocessor = function(args, logger, config) {
+
+  var log = logger.create('preprocessor.custom');
+
+  var script = (config && config.script) || null;
+
+  var stopOnError = (config && config.stopOnError) || false;
+
+  var messageOnError = (config && config.messageOnError) || null;
+
+  return function(content, file, done) {
+    var result = null;
+
+    if (script) {
+      result = shell.exec(script);
+    }
+
+    log.info('shell execution result', result);
+
+    if (result && result.code !== 0 && stopOnError) {
+      if (messageOnError) {
+        log.error(messageOnError);
+      }
+      done(new Error(result.output), null);
+    } else {
+      done(null, content);
+    }
+
+  };
+};
+
+runScriptOnChangePreprocessor.$inject = ['args', 'logger', 'config.runscript'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'preprocessor:runscript': ['factory', runScriptOnChangePreprocessor]
+};
